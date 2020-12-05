c786e852f68635c82014c8b8072ab7540628517f
Mariandrea Del Boccio <mariandreadelboccio@AirdeMariandrea.technicolor.net>
2020-12-05 17:04:49 +0100

    db added

--- src/keys.js
+++ src/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
   database: {
-    URI: process.env.MONGODB_URI || 'mongodb://localhost/creatist-db'
+    URI: process.env.MONGODB_URI || 'mongodb+srv://maridelboccio:24896983@cluster0.oytjq.mongodb.net/creatist-db?retryWrites=true&w=majority'
   }
 };
